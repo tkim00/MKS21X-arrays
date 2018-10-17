86580e8fc86cdb6b5fe11aba4bf459f5e38d40e8
Taejoon Kim <tkim00@stuy.edu>
2018-10-16 23:46:51 -0400

    almost done

---
+++ ArrayDemo.java
@@ -0,0 +1,54 @@
+public class ArrayDemo{
+	public static void printArray(int[]ary){
+		System.out.print("{ ");
+		for(int i = 0; i < ary.length; i++){
+			System.out.print("" + ary[i] + ",");
+		}
+		System.out.print(" }");
+	}
+	public static void printArray(int[][]ary){
+		System.out.print("{ ");
+		for(int x = 0; x < ary.length; x++){
+			System.out.println("{ ");
+			for(int y = 0; y < ary[x].length; y++){
+				System.out.print("" + ary[x][y] + ",");
+			}
+			System.out.print(" }");
+		}
+		System.out.print(" }");
+	}
+	public static int countZeros2D(int[][] nums){
+		int c = 0;
+                for(int x = 0; x < nums.length; x++){
+                        for(int y = 0; y < nums[x].length; y++){
+				if(nums[x][y] == 0){
+                                c++;
+				}
+                        }
+                }
+		return c;
+	}
+	public static void fill2D(int[][] vals){
+		for(int x = 0; x < vals.length; x++){
+                        for(int y = 0; y < vals[x].length; y++){
+				vals[x][y] = 1;
+                                if(x == y){
+                                vals[x][y] = 3;
+                                }
+                        }
+                }
+	}
+/*
+	public static int[][] fill2DCopy(int[][] vals){
+		int[][] ary = new int[vals.length][vals[].length];
+		for(int x = 0; x < vals.length; x++){
+                        for(int y = 0; y < vals.length; y++){
+                                if(vals[x][y] == 0){
+                                c++;
+                                }
+                        }
+                }
+	}
+*/
+}
+
