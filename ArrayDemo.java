902661e8e3d72fc2d39055d3e30939caf0670742
Taejoon Kim <tkim00@stuy.edu>
2018-10-23 19:56:40 -0400

    compiled and debugged

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,25 +1,46 @@
 public class ArrayDemo{
+	public static void main(String[] args){
+    int[] ary1 = new int[10];
+    for(int i = 0; i < ary1.length; i++){
+      ary1[i] = i;
+    }
+    int[][] ary2 = new int[10][10];
+    for(int x = 0; x < ary2.length; x++){
+      for(int y = 0; y < ary2.length; y++){
+        ary2[x][y] = x * y;
+      }
+    }
+
+    printArray(ary1);
+    printArray(ary2);
+    int x = countZeros2D(ary2);
+		System.out.println(x);
+    fill2D(ary2);
+    printArray(ary2);
+    fill2DCopy(ary2);
+    printArray(ary2);
+  }
 	public static void printArray(int[]ary){
 		System.out.print("{ ");
 		for(int i = 0; i < ary.length; i++){
 			System.out.print("" + ary[i] + ",");
 		}
-		System.out.print(" }");
+		System.out.print(" }\n");
 	}
 	public static void printArray(int[][]ary){
 		System.out.print("{ ");
 		for(int x = 0; x < ary.length; x++){
-			System.out.println("{ ");
+			System.out.print("{ ");
 			for(int y = 0; y < ary[x].length; y++){
 				System.out.print("" + ary[x][y] + ",");
 			}
-			System.out.print(" }");
+			System.out.print(" }\n");
 		}
-		System.out.print(" }");
+		System.out.print(" }\n");
 	}
 	public static int countZeros2D(int[][] nums){
 		int c = 0;
-                for(int x = 0; x < nums.length; x++){
+               for(int x = 0; x < nums.length; x++){
                         for(int y = 0; y < nums[x].length; y++){
 				if(nums[x][y] == 0){
                                 c++;
@@ -39,7 +60,7 @@ public class ArrayDemo{
                 }
 	}
 	public static int[][] fill2DCopy(int[][] vals){
-		int[][] ary = new int[vals.length][vals[].length];
+		int[][] ary = new int[vals.length][vals[0].length];
 		for(int x = 0; x < vals.length; x++){
                         for(int y = 0; y < vals.length; y++){
                                 ary[x][y] = 1;
@@ -51,4 +72,3 @@ public class ArrayDemo{
 		return ary;
 	}
 }
-
